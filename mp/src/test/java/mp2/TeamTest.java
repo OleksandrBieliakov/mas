835aab990f02cff64a171fc167978b544e8bbb68
Oleksandr Bieliakov <abel9k@gmail.com>
2021-04-25 10:23:53 +0200

    fixed issues

--- mp/src/test/java/mp2/TeamTest.java
+++ mp/src/test/java/mp2/TeamTest.java
@@ -111,4 +111,44 @@ class TeamTest {
         assertFalse(Task.getExtent().contains(task1));
         assertFalse(Task.getExtent().contains(task2));
     }
+
+    @Test
+    void deleteAssignment() {
+        Team team1 = new Team("team1");
+        Task task1 = new Task("task1", team1);
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+
+        assertEquals(0, task1.getAssignments().size());
+        assertEquals(0, user1.getTasksAssignedBy().size());
+        assertEquals(0, user1.getTasksAssignedTo().size());
+        assertEquals(0, user2.getTasksAssignedBy().size());
+        assertEquals(0, user2.getTasksAssignedTo().size());
+
+        Assignment assignment1 = new Assignment(task1, user1, user2);
+        assertEquals(1, task1.getAssignments().size());
+        assertEquals(1, user1.getTasksAssignedBy().size());
+        assertEquals(0, user1.getTasksAssignedTo().size());
+        assertEquals(0, user2.getTasksAssignedBy().size());
+        assertEquals(1, user2.getTasksAssignedTo().size());
+        assertTrue(Assignment.getExtent().contains(assignment1));
+
+        Assignment assignment2 = new Assignment(task1, user1, user1);
+
+        assertEquals(2, task1.getAssignments().size());
+        assertEquals(2, user1.getTasksAssignedBy().size());
+        assertEquals(1, user1.getTasksAssignedTo().size());
+        assertEquals(0, user2.getTasksAssignedBy().size());
+        assertEquals(1, user2.getTasksAssignedTo().size());
+        assertTrue(Assignment.getExtent().contains(assignment2));
+
+        Team.delete(team1);
+        assertEquals(0, user1.getTasksAssignedBy().size());
+        assertEquals(0, user1.getTasksAssignedTo().size());
+        assertEquals(0, user2.getTasksAssignedBy().size());
+        assertEquals(0, user2.getTasksAssignedTo().size());
+        assertFalse(Task.getExtent().contains(task1));
+        assertFalse(Assignment.getExtent().contains(assignment1));
+        assertFalse(Assignment.getExtent().contains(assignment2));
+    }
 }
